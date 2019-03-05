c1a8ded92c2506846c11292df47a38edda08f9c1
Armin Blaze <box_for_spam@mail.ru>
2019-03-05 23:43:01 +0300

    30 - Передаём объект в компонент через {...spread}. Т.е. создаём копию объекта

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,9 +4,12 @@ import TodoListItem from 'components/TodoListItem';
 const TodoList = ({todos}) => {
 	
 	const elements = todos.map( (item, i) => {
+		//используем деструктуризацию. Т.к. label={item.label} important={item.important} - Т.е. имена передаваемых свойств (label и important) Совпадают с именами свойств item.label и item.important. То мы передаём в компонент просто объект item
+//		Либо можно вызывать компонент как обычную функцию
+//		{TodoListItem(item)}
 		return (
 			<li>
-				<TodoListItem label={item.label} important={item.important} />
+				<TodoListItem {...item} />
 			</li>
 		)
 	});
