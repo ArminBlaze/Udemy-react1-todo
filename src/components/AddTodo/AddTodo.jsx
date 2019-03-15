815bb67dd0c668e9670fd80f37dc8a5a51a7d0a9
Armin Blaze <box_for_spam@mail.ru>
2019-03-15 16:44:34 +0300

    44 - Форма, куда можно ввести имя новой задачи

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -3,14 +3,43 @@ import './AddTodo.css';
 
 class AddTodo extends React.Component {
 	
+	constructor() {
+		super();
+		
+		this.state = {
+			label: '',
+		}
+	}
+	
+	
+	onLabelChange(e) {
+		console.log(e.target.value);
+		
+		this.setState(
+			{
+				label: e.target.value
+			}
+		)
+	}
+	
+	onFormSubmit(e) {
+		e.preventDefault();
+		this.props.onTodoAdd(this.state.label);
+		e.target.reset();
+	}
+	
 	render() {
 		return (
-		<div className="AddTodo d-flex">
-			<button type="button"
+		<form className="AddTodo d-flex" onSubmit={ this.onFormSubmit.bind(this) }>
+			<input type="text"
+				className="form-control"
+				onChange={ this.onLabelChange.bind(this) }
+				placeholder="Что ещё нужно сделать?"
+			/>
+			<button type="submit"
 			className="btn btn-info"
-			onClick={ () => this.props.onTodoAdd('Hello Peka!') }
-			>Добавить элемент</button>
-		</div>
+			>Добавить</button>
+		</form>
 		);
 	}
 }
