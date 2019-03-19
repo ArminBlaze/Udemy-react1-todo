d3049761836c2018bee3ad373b4a9b5ecad8a838
Armin Blaze <box_for_spam@mail.ru>
2019-03-19 20:07:33 +0300

    Фильтрация кнопками через react обработчики

--- src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -9,11 +9,17 @@ class ItemStatusFilter extends React.Component {
 		
 		this.state = {
 			activeFilters: 'all',
+			buttons: [
+				{name: 'all', label: 'All'},
+				{name: 'active', label: 'Active'},
+				{name: 'done', label: 'Done'},
+			]
 		}
 	}
 	
-	onButtonsClick(e) {
-		let filter = e.target.textContent.toLowerCase();
+	onButtonsClick(filter) {
+		console.log(filter);
+//		let filter = e.target.textContent.toLowerCase();
 		
 		this.setState({
 			activeFilters: filter
@@ -31,17 +37,21 @@ class ItemStatusFilter extends React.Component {
 	}
 	
 	render() {
+		
+		let buttons = this.state.buttons.map(item => {
+			return (
+				<button type="button"
+              className={this.setClassForButton(item.name)}
+              key={item.name}
+              onClick={ () => { this.onButtonsClick(item.name) } }
+              >{item.label}</button>
+			)
+		})
+		
+		
 		return (
-    <div className="btn-group" onClick={ this.onButtonsClick.bind(this) }>
-      <button type="button"
-              className={this.setClassForButton('all')}
-              >All</button>
-      <button type="button"
-              className={this.setClassForButton('active')}
-              >Active</button>
-      <button type="button"
-              className={this.setClassForButton('done')}
-              >Done</button>
+    <div className="btn-group">
+      {buttons}
     </div>
   );
 	}
